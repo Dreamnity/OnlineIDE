26e6d849933ce91ecfdbf9642b0f108814774add
5UP3R_D1N <superdinmc@gmail.com>
2024-09-24 20:55:53 +0000

    Websocket support

--- index.ts
+++ index.ts
@@ -1,15 +1,16 @@
 import { join } from "node:path";
-import linguist from "linguist-js";
+import { ModelOperations } from "@vscode/vscode-languagedetection";
 import piston from "piston-client";
 
 const acePath = join(__dirname, "ace-builds", "src-min");
 const modulePath = join(__dirname, "node_modules");
 const sauce = join(__dirname, "sauce");
 const errorPage = await Bun.file(join(sauce, "404.html")).text();
-const client = piston({ server: "http://2.dreamnity.in:2000" });
+const server = "http://2.dreamnity.in:2000/api/v2/connect";
+const classifier = new ModelOperations();
 
-console.log("\u001bcIt works", Date.now()%10000);
-Bun.serve({
+console.log("\u001bcIt works", Date.now() % 10000);
+Bun.serve<ExecutionData>({
   async fetch(req) {
     const url = new URL(req.url);
     // Modules
@@ -19,26 +20,26 @@ Bun.serve({
     if (url.pathname.startsWith("/ace"))
       return new Response(Bun.file(join(acePath, url.pathname.split("/").slice(2).join("/"))));
     if (url.pathname.startsWith("/api/classify")) {
-      const { files } = await linguist("file", {
-        fileContent: await req.text(),
-        //offline: true,
-        //categories: ["programming"]
-      });
-      if (!files.results["file"]) return new Response("null", {
+      const result = await classifier.runModel(await req.text());
+      console.log(result[0])
+      if (!result?.[0]) return new Response("null", {
         status: 404
       });
-      return new Response(files.results["file"]);
+      return new Response(result[0].languageId);
     }
     if (url.pathname.startsWith("/api/execute")) {
-      return new Response(
-        JSON.stringify(
-          await client.execute(url.searchParams.get("lang") || 'javascript', await req.text(), {
-            version: 'latest'
-          })
-        )
-      )
+      /*const result = await client.execute(, await req.text(), {
+        version: '*'
+      });*/
+      if (!this.upgrade(req, {
+        data: {
+          lang: url.searchParams.get("lang") || 'js'
+        }
+      })) return new Response("Invalid request", {
+        status: 400
+      });
     }
-    let path = join(sauce,url.pathname);
+    let path = join(sauce, url.pathname);
     if (path.endsWith("/") && await Bun.file(path + "index.html").exists())
       return new Response(Bun.file(path + "index.html"));
     const file = Bun.file(path);
@@ -47,6 +48,72 @@ Bun.serve({
   },
   error(req) {
     return makeErrorPage(req.message);
+  },
+  websocket: {
+    open(ws) {
+      ws.send("!!ready");
+    },
+    message(ws, msg) {
+      const message = msg.toString();
+      if (ws.data.code === undefined) {
+        ws.data.code = message;
+
+        const back = ws.data.conn = new WebSocket(server);
+        back.onopen = () => {
+          ws.send("!!starting");
+          back.send(JSON.stringify({
+            type: "init",
+            language: ws.data.lang,
+            version: "*",
+            files: [{
+              content: ws.data.code
+            }],
+            run_timeout: 600000,
+            run_memory_limit: 5e+7
+          }));
+        };
+        back.onmessage = msg => {
+          const data = JSON.parse(msg.data.toString());
+          switch (data?.type) {
+            case 'stage':
+              if (data?.stage == 'run') ws.send("!!running");
+              break;
+            case 'data':
+              ws.send(data.data);
+              break;
+            case 'exit':
+              ws.send("\x1b[34m[exit: " + (data.signal || data.code) + "]\x1b[0m");
+              if(data.code) ws.send("!!exit "+(data.code||0));
+              ws.close();
+              back.close();
+              break;
+            case 'error':
+              ws.send("\x1b[31m[error: " + data.message + "]\x1b[0m");
+              ws.send("!!exit (internal error)");
+              ws.close();
+              back.close();
+              break;
+            default:
+              return;
+          }
+        }
+      }
+      if (ws?.data?.conn?.readyState !== 1) return;
+      ws.data.conn.send(JSON.stringify({
+        type: "data",
+        stream: "stdin",
+        data: message
+      }));
+    },
+    close(ws) {
+      if (ws?.data?.conn?.readyState === 1) {
+        ws.data.conn.send(JSON.stringify({
+          type: "signal",
+          signal: "SIGKILL"
+        }));
+        ws.data.conn.close();
+      }
+    }
   },
   port: 50004
 });
@@ -61,8 +128,8 @@ function makeErrorPage(message: string, code: number = 500) {
   });
 }
 
-type Runtime = {
-  language: string,
-  version: string,
-  aliases: string[]
-}
+interface ExecutionData {
+  lang: string,
+  code: string,
+  conn: WebSocket
+};
