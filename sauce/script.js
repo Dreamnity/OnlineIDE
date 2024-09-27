51d255000e5ebafe7ef3036fe0b3097a8163b208
Dreamgineer <superdinmc@dreamnity.in>
2024-09-27 20:46:20 +0000

    Connectivity checker

--- sauce/script.js
+++ sauce/script.js
@@ -44,9 +44,13 @@ OnlineIDE@0.1.2-BETA
   fit.fit();
   term.write(motd);
   fetch("/api/changelog")
-    .then(e => e.text())
-    .then(e => term.write("\n\n  Live changelog\n\x1b[34m" + e + "\x1b[0m"))
-    .catch(() => { });
+    .then(async e => {
+      if (e.status !== 200) term.write("\n\n\x1b[31mCannot request to OnlineIDE server(Are you offline?)");
+      else term.write("\n\n  Live changelog\n\x1b[34m" + (await e.text()) + "\x1b[0m");
+    })
+    .catch(() => {
+      term.write("\n\n\x1b[31mCannot request to OnlineIDE server(Are you offline?)");
+    });
   editor.session.on("change", () => {
     clearTimeout(window?.saveInterval);
     window.saveInterval = setTimeout(detect, auto ? 500 : 5000);
@@ -80,21 +84,6 @@ OnlineIDE@0.1.2-BETA
     document.getElementById("lang").innerHTML = this?.innerHTML || name;
   }
   window.set = setLang;
-  fetch("/api/list").then(r => r.json()).then(l => {
-    const dropdown = document.querySelector("ul.dropdown-menu");
-    const regex = /[^a-zA-Z0-9\-_]/g
-    for (const { language, aliases, version } of l) {
-      const id = (regex.test(language) ? aliases.find(e => !regex.test(language)) || language : language).replace(/\+/g, "c");
-      const li = document.createElement("li");
-      const a = document.createElement("a");
-      a.className = 'dropdown-item';
-      a.id = 'lang-' + id;
-      a.innerHTML = `${language} <sup>${version}</sup>`;
-      a.onclick = () => setLang(id);
-      li.appendChild(a);
-      dropdown.appendChild(li);
-    }
-  }).finally(()=>document.getElementById("splash").hidden = true);
   /**
    * @type {WebSocket}
    */
@@ -150,6 +139,28 @@ OnlineIDE@0.1.2-BETA
       ws = null;
     }
   }
+  fetch("/api/list").then(async r => {
+    if (r.status !== 200) {
+      runBtn.classList.add("btn-danger");
+      runBtn.disabled = true;
+      runBtn.innerText = "OFFLINE";
+      return;
+    }
+    const l = await r.json();
+    const dropdown = document.querySelector("ul.dropdown-menu");
+    const regex = /[^a-zA-Z0-9\-_]/g
+    for (const { language, aliases, version } of l) {
+      const id = (regex.test(language) ? aliases.find(e => !regex.test(language)) || language : language).replace(/\+/g, "c");
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+      a.className = 'dropdown-item';
+      a.id = 'lang-' + id;
+      a.innerHTML = `${language} <sup>${version}</sup>`;
+      a.onclick = () => setLang(id);
+      li.appendChild(a);
+      dropdown.appendChild(li);
+    }
+  }).finally(() => document.getElementById("splash").hidden = true);
   const search = new URLSearchParams(location.search);
   if (search.has("code")) {
     const code = atob(search.get("code"));
